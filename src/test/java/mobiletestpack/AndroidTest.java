4d6ad96529ab758f0ec49dc3f932431c58438937
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2024-01-23 20:55:47 +0300

    Appium session cont

--- src/test/java/mobiletestpack/AndroidTest.java
+++ src/test/java/mobiletestpack/AndroidTest.java
@@ -25,10 +25,16 @@ public class AndroidTest {
 //        options.setApp("/Users/aravindbalaji/Documents/Appium/Sample App/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
 
         // calling the andorid driver to run the app
-        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),options);
-Thread.sleep(3000);
-    System.out.println("is displayed check : " + driver.findElement(AppiumBy.cssSelector("android.widget.TextView[text='Sauce Labs Backpack']")).isDisplayed());
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
         Thread.sleep(3000);
-    System.out.println("is enabled check : " + driver.findElement(AppiumBy.cssSelector("android.widget.TextView[text='Sauce Labs Backpack'][content-desc='store item text']")).isEnabled());
+        System.out.println("is displayed check : " + driver.findElement(AppiumBy.cssSelector("android.widget.TextView[text='Sauce Labs Backpack']")).isDisplayed());
+        Thread.sleep(3000);
+        System.out.println("is enabled check : " + driver.findElement(AppiumBy.cssSelector("android.widget.TextView[text='Sauce Labs Backpack'][content-desc='store item text']")).isEnabled());
+
+        Thread.sleep(5000);
+
+        System.out.println("check the display :"+ driver.findElement(AppiumBy.cssSelector("android.view.ViewGroup[content-desc='store item'] android.widget.TextView[text='Sauce Labs Backpack']")).isDisplayed());
+        System.out.println("check the display :"+ driver.findElement(AppiumBy.xpath("//android.view.ViewGroup[@content-desc='store item']//android.widget.TextView[@text='Sauce Labs Backpack']")).isDisplayed());
+
     }
 }
