3441334db532b4eac64b3f9c99580b028995f3c4
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2025-03-20 20:18:17 +0300

    appium session capabilities

--- src/test/java/handlingcapabilities/CapabilitiesCheck_Ios.java
+++ src/test/java/handlingcapabilities/CapabilitiesCheck_Ios.java
@@ -15,16 +15,21 @@ public class CapabilitiesCheck_Ios {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
         XCUITestOptions options = new XCUITestOptions();
         options.setPlatformName("IOS");
-//        options.setBundleId("com.moataz.dailycheck");
+        options.setBundleId("com.moataz.dailycheck");
         options.setAutomationName(AutomationName.IOS_XCUI_TEST);
 //        options.setApp("/Users/aravindbalaji/Documents/Appium/Sample App/android-app.apk");
-        options.setApp("/Users/aravindbalaji/Documents/Appium/SampleApp/iOS-Simulator-MyRNDemoApp.1.3.0-162.zip");
+//        options.setApp("/Users/aravindbalaji/Documents/Appium/SampleApp/iOS-Simulator-MyRNDemoApp.1.3.0-162.zip");
 
         options.setUdid("9C4CF5F8-F6CD-44FC-AC73-5B595592AD1B");
-        options.setAutoAcceptAlerts(true);
+        options.setAutoAcceptAlerts(true);// any pop up comes
 //        options.withBrowserName("Safari");
         options.setUseNewWDA(false); // this will not install webdriver agent agiain and again it will try to re-use if the webdriver agent is valid
-        options.setNewCommandTimeout(Duration.ofSeconds(40));
+        options.setForceAppLaunch(true);// for app to launch
+        options.setNewCommandTimeout(Duration.ofSeconds(40));// any command that will be execute will have default timed out - max time
+        options.setCapability("","");// any custom capability for hte app you can mentionhere
+
+//        options.setDeviceName("");// Not recomended to use instead use setUdid - Andrioid if we do not meniton the udid it might run on any device that is connnect
+
         options.setFullReset(true);
 
 
