4c01e00c6e1622ea78c11c1221ecce99cc8d1cae
AravindBalaji1990 <aravindbalaji.balaji7@gmail.com>
2025-03-21 20:11:45 +0300

    appium session gestures

--- src/test/java/handlinggestures/HandlingGestures_swipe_leftright.java
+++ src/test/java/handlinggestures/HandlingGestures_swipe_leftright.java
@@ -24,17 +24,11 @@ import java.util.Collections;
 public class HandlingGestures_swipe_leftright {
 
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
-
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
-//        options.setDeviceName("29221JEGR00379");
         options.setUdid("29221JEGR00379");
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-//        options.setApp("");
-        // capability to allow the location based permission or any default permission
-        options.setAutoGrantPermissions(true);
-//        options.setAppWaitForLaunch(true);
-//        options.setAppWaitDuration(Duration.ofMillis(50000));
+        options.setAutoGrantPermissions(true);// to allow the location
         options.setAppPackage("co.motovolt.motovoltapp");
         options.setAppActivity("co.motovolt.motovoltapp.MainActivity");
 
@@ -42,55 +36,36 @@ public class HandlingGestures_swipe_leftright {
         // calling the andorid driver to run the app̵
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
 
-        //        WebElement element = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup[@content-desc='open menu']/android.widget.ImageView"));
-//        WebElement element = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Views\"]\n"));
-//        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.elementToBeClickable(element));
-//        element.click();
-        Thread.sleep(5000);
-        //Scroll/swipe action
-        WebElement ele = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup/following-sibling::android.widget.TextView[@text='Drive Less, e-Cycle More!']/preceding-sibling::android.view.ViewGroup/android.widget.ImageView"));
 
+        //we are going to swipe on a specific element to do swipe gestures
+        WebElement ele = driver.findElement(AppiumBy.xpath("//android.view.ViewGroup/following-sibling::android.widget.TextView[@text='Drive Less, e-Cycle More!']/preceding-sibling::android.view.ViewGroup/android.widget.ImageView"));
+        // custom function
         swipeLeft(driver, ele);
 
-        Thread.sleep(5000);
+//        Thread.sleep(5000);
 
-//
-        WebElement ele2 = driver.findElement(AppiumBy.xpath("//android.widget.HorizontalScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.widget.ImageView"));
-        swipeLeft(driver, ele2);
+//        WebElement ele2 = driver.findElement(AppiumBy.xpath("//android.widget.HorizontalScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.widget.ImageView"));
 //        swipeRight(driver, ele2);
 
-        WebElement ele3 = driver.findElement(AppiumBy.xpath("//android.widget.HorizontalScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageView"));
-        swipeLeft(driver, ele3);
+//        WebElement ele3 = driver.findElement(AppiumBy.xpath("//android.widget.HorizontalScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageView"));
+//        swipeLeft(driver, ele3);
 
-        System.out.println(driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"REGISTER\"]")).isDisplayed());
+//        System.out.println(driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"REGISTER\"]")).isDisplayed());
 
+//        WebElement ele4 = driver.findElement(AppiumBy.xpath("//android.widget.ImageView"));
+//        swipeRight(driver, ele4);
 
         Thread.sleep(5000);
         driver.quit();
-//        Point midpoint = new Point((int)(size.width*0.5), (int)(size.height*0.5));
-//
-//        int left = midpoint.x - (int)(midpoint.x *0.75);
-//        int right = midpoint.x + (int)(midpoint.x *0.75);
-//
-//        Point start = new Point(right, midpoint.y);
-//        Point end = new Point(left, midpoint.y);
-        //Starting x location set to 5% of the width (near left)
-//        int startx = (int) (size.width * 0.05);
-//        //y position set to mid-screen vertically
-//        int starty = size.height / 2;
-//        //Ending x location set to 95% of the width (near right)
-//        int endx = (int) (size.width * 0.95);
-//        int endy = starty;
-
-
-
     }
 
 
-    public static void swipeLeft(AndroidDriver driver , WebElement ele){
+    public static void swipeLeft(AndroidDriver driver, WebElement ele) {
         int startX = ele.getRect().x + (ele.getSize().width * 3 / 4);
+        System.out.println(startX);
         int startY = ele.getRect().y + (ele.getSize().height / 2);
         int endX = ele.getRect().x + (ele.getSize().width / 4);
+        System.out.println(endX);
         int endY = ele.getRect().y + (ele.getSize().height / 2);
 
         PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "touch1");
@@ -101,11 +76,11 @@ public class HandlingGestures_swipe_leftright {
                 .addAction(finger1.createPointerMove(Duration.ofMillis(100), PointerInput.Origin.viewport(), endX, endY))
                 .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
         driver.perform(Arrays.asList(seq));
-        System.out.println("-----Swipe Ended-----");
+        System.out.println("-----Swipe left Ended-----");
     }
 
 
-    public static void swipeRight(AndroidDriver driver , WebElement ele){
+    public static void swipeRight(AndroidDriver driver, WebElement ele) {
         int startX = ele.getRect().x + (ele.getSize().width / 4);
         int startY = ele.getRect().y + (ele.getSize().height / 2);
         int endX = ele.getRect().x + (ele.getSize().width * 3 / 4);
@@ -119,6 +94,6 @@ public class HandlingGestures_swipe_leftright {
                 .addAction(finger1.createPointerMove(Duration.ofMillis(100), PointerInput.Origin.viewport(), endX, endY))
                 .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
         driver.perform(Arrays.asList(seq));
-        System.out.println("-----Swipe Ended-----");
+        System.out.println("-----Swipe right Ended-----");
     }
 }
